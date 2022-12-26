a974e4661f0c95d19ea2c1be17dc1f956d462386
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2022-12-26 15:23:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 
 /**
- * main - "prints Programming is like building a multilingual puzzl, followed by a newlinee"
- * return 0.
+* main - "prints Programming is like building a multilingual puzzl, followed by a newlinee"
+* return 0.
 */
 
 int main(void)
