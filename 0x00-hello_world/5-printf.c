1c21be5e390fbe4d0093bf9afa56a5a2716a336a
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2022-12-27 00:51:59 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammer, but the outcome is a piece of art,"
+ * main - prints "with proper grammer, but the outcome is a piece of art,",
  * followed newline
  * Return: Always 0.
  */
