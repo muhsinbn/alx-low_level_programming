adbfcd419e7ee748ad5155002d0f91cd87e85403
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2022-12-26 23:12:34 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,15 @@
 
 
 /**
-* main - "prints Programming is like building a multilingual puzzl, followed by a newlinee"
-* return 0.
+* main - "prints Programming is like building a multilingual puzzle, followed by a newline."
+* Return: Always return the value 0.
 */
 
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+	return(0);
 
 }
