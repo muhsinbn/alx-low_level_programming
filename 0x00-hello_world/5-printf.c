c5a72b4992560bec1998c2f4e950fd5cddb22578
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2022-12-27 00:41:53 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- *main - prints "with a proper grammer, but the outcome is a piece of art,"
- *followed by a new line
- *  Return: Always 0.
+ * main - prints "with proper grammer, but the outcome is 
+ * a piece of art," followed newline
+ * Return: Always 0.
+ *
  */
 
 int main(void)
