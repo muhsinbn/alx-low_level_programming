6cad4e83ef52952f252ba0eb42afcd51e4c2ac18
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2022-12-28 15:18:28 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include  "main.h"
 
 /**
- * print_alphabet_x10 - make the alphabet x10 
- * Return: void
+ *print_alphabet_x10 - make the alphabet x10 
+ *Return: void
  */
 void print_alphabet_x10(void)
 
