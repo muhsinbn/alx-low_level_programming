7962f55e37a8c16ec76c901dc3cb06c793005f4c
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2022-12-28 15:12:05 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,22 @@
-#include "main.h"
+#include  "main.h"
+
 /**
- *peint_alphabet_x10 - Make alphabet x10 times
- *Return:void
- void print_alphabet_x10(void)
- {
- char c;
- int i = 0;
- while (i <= 0);
- for (c = 'a'; c <= 'z'; c++)
- {
- _putchar(c);
- _putchar('\n');
- t++;
- }
- }
+ * print_alphabet_x10 - make the alphabet x10 
+ * Return: void
+ */
+void print_alphabet_x10(void)
+
+{
+	char c;
+	int i = 0;
+
+	while (i <= 0)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
