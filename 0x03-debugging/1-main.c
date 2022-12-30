38621fc2951d454739e8c1ec083dbc6345f13764
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2022-12-29 23:16:03 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+
 /**
  * main - create an infinte loop
  * Return: return the value of infinte loop
  */
+
+
+
 int main(void)
+
 {
+
 	int i;
 
 	printf("infinite loop incomming :(\n");
-
 	i = 0;
-
-	/* while (i < 10 );*/
-	{
-	putchar(i);
-	}
+	/*while (i < 10 )*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
 	printf("infinite loop avoided! \\o/\n");
 	return (0);
 }
