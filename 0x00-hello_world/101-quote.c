04c7511d4ebacaf1cef5b408b06531966879f74e
Mohammed <muhsinbnyahya@gmail.com>
2023-07-16 07:33:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
-
 /**
- * main - Entru point
+ * main - Entry point
  *
- * Return: Always 0 (successful)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19, 59");
-	return(1);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+	return (1);
 }
