e7f35360c19ad69cf19aa91905d0495cd2f0c294
Mohammed <muhsinbnyahya@gmail.com>
2023-07-17 10:56:01 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,10 +26,10 @@ int main(void)
 					{
 						putchar(h);
 						putchar(g);
-						pitchar(' ');
+						putchar(' ');
 						putchar(e);
 						putchar(i);
-						if (h == 57 && g ==56 && e == 57 && i == 57)
+						if (h == 57 && g == 56 && e == 57 && i == 57)
 							break;
 						putchar(',');
 						putchar(' ');
