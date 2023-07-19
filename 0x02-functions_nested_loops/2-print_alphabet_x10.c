ea55ed895e3d1360ee214fb82cce0b0b438f6777
Mohammed <muhsinbnyahya@gmail.com>
2023-07-18 17:20:40 -0700

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,9 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-													_putchar(letter);
-		_putchar('\n');
-												}
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+	_putchar('\n');
+											}
 }
