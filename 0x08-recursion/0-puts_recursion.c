be06c9f5192033a9f1a3247d1bcd7717f5862f30
Mohammed <muhsinbnyahya@gmail.com>
2023-08-02 10:31:09 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,10 +10,10 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
+	_putchar('\n');
 	return;
-}
-_putchar(*s);
-s++;
-_puts_recursion;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
