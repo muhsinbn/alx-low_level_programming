043169586ee15ffe5d8e8d05c002b88c0e1fe86c
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-05-03 17:38:30 -0700

    code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# Creates a file
-file { '/tmp/school':
-  ensure  => present,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
+# Create file in /tmp
+
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+
